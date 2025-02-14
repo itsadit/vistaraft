14b88179e1130f7ff779139bea51690093d9ca10
Anurag ghosh <ag952006@student.nitw.ac.in>
2025-02-14 17:20:59 +0530

    menu button updated

--- vistaraft/src/components/Header/Header.jsx
+++ vistaraft/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import logo from "@/assets/logoVista.jpg";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import menu from "@/assets/menu.png";
 
 function Header() {
+  const location = useLocation();
   const toggleNavbar = () => {
     const navbar = document.getElementById('navbar-default');
     const isExpanded = navbar.classList.contains('hidden');
@@ -10,9 +12,19 @@ function Header() {
     } else {
       navbar.classList.add('hidden');
     }
+
   };
+
+const toggleSpan = () => {
+  const homeBtn = document.getElementById('homeBtn');
+  const homeSpan = document.getElementById('homeSpan');
+  if(homeBtn.classList.contains('active')){
+    homeSpan.classList.remove('hidden');
+    
+  }
+};
   return (
-    <nav className="sticky top-0 left-0 w-full bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 bg-opacity-95 backdrop-blur-md border-b border-transparent z-50">
+    <nav className="sticky  top-0 left-0 w-full bg-white bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 bg-opacity-95 backdrop-blur-md border-b border-transparent z-50">
 
       <div className="max-w-screen-xl flex justify-between items-center mx-auto p-4">
         {/* Logo and Brand Name */}
@@ -24,74 +36,68 @@ function Header() {
         </a>
 
         {/* Mobile Menu Button */}
+        <div className="flex-col justify-end ">
+        <div className="flex justify-end ">
         <button
         id="navBtn"
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600"
+          className="md:hidden w-10 pb-2 pl-0 pr-0 h-10 hover:border-none bg-cyan-600 rounded-lg focus:outline-none focus:ring-0"
           aria-controls="navbar-default"
           aria-expanded="false"
           onClick={toggleNavbar}
+          style={{backgroundImage: `url(${menu})`,backgroundPosition: 'center',backgroundSize: 'cover', backgroundColor: 'transparent'}}
         >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
+          
+          
         </button>
+        </div>
+          
 
         {/* Navigation Links */}
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-            <li>
+        <div className="hidden  md:block" id="navbar-default">
+          <ul className="w-full font-medium flex flex-col p-2 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+            <li className="w-auto">
               <Link
                 to="/"
-                className="nav-link block py-2 px-3 text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
+                id="homeBtn"
+                className="nav-link block text-right py-2 px-3 text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
                 aria-current="page"
+                onClick={toggleSpan}
               >
                 Home
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-cyan-300 transition-all duration-300 group-hover:w-full"></span>
+                <span id="homeSpan" className="hidden absolute bottom-0 left-0 w-0 h-0.5 bg-cyan-300 transition-all duration-300 w-full"></span>
               </Link>
             </li>
-            <li>
+            <li className="w-auto">
               <Link
                 to="tel:+8384076491"
-                className="nav-link block py-2 px-3 text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
+                className="nav-link block text-right py-2 px-3 text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
               >
                 Call Us
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-cyan-300 transition-all duration-300 group-hover:w-full"></span>
+                <span className="hidden absolute bottom-0 left-0 w-0 h-0.5 bg-cyan-300 transition-all duration-300 group-hover:w-full"></span>
               </Link>
             </li>
-            <li>
+            <li className="w-auto">
               <Link
                 to="/about"
-                className="nav-link block py-2 px-3 text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
+                className="nav-link block text-right py-2 px-3 text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
               >
                 About Us
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-cyan-300 transition-all duration-300 group-hover:w-full"></span>
+                <span className="absolute bottom-0  left-0 w-0 h-0.5 bg-cyan-300 transition-all duration-300 group-hover:w-full"></span>
               </Link>
             </li>
-            <li>
+            <li className="w-auto">
               <Link
                 to="/contact"
-                className="nav-link block py-2 px-3 text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
+                className="nav-link block py-2 px-3 text-right text-white hover:text-cyan-300 rounded-md relative overflow-hidden transition-all duration-300 md:p-0"
               >
                 Contact Us
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-cyan-300 transition-all duration-300 group-hover:w-full"></span>
               </Link>
             </li>
           </ul>
+        </div>
         </div>
       </div>
     </nav>
